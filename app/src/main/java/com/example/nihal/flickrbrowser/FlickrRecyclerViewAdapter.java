35f301ce81cee8be841dd4602b24b7b6a0b97b82
nihal saxena <saxenanihal95@gmail.com>
2017-06-22 14:58:29 +0530

    added FlickrRecylerViewAdapder and FlickerImageViewHolder

---
+++ app/src/main/java/com/example/nihal/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,68 @@
+package com.example.nihal.flickrbrowser;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by nihal on 22/6/17.
+ */
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder>{
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotoList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter( Context context,List<Photo> photoList) {
+        mContext = context;
+        mPhotoList = photoList;
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Called by the layout manager when it needs a new view
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse,parent,false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+
+    }
+
+    void loadNewData(List<Photo> newPhotos){
+        mPhotoList=newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position){
+        return ((mPhotoList!=null)&&(mPhotoList.size()!=0)?mPhotoList.get(position):null);
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((mPhotoList!=null) && (mPhotoList.size()!=0)?mPhotoList.size():0);
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder{
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail=null;
+        TextView title =null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail=(ImageView)itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView)itemView.findViewById(R.id.title);
+        }
+    }
+}
